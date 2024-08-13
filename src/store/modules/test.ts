ca9f4659b0bbe36508bc2d5402857916b387ec73
Kms <kms@inesa-t.com>
2024-08-13 11:18:25 +0800

    chore(store): 🐳 添加store

---
+++ src/store/modules/test.ts
@@ -0,0 +1,18 @@
+import { defineStore } from 'pinia'
+
+export const useTestStore = defineStore('test', {
+  state: () => {
+    return {
+      list: [1, 2, 3],
+    }
+  },
+  getters: {
+    index: (state) => state.list.map((v) => v + 'name'),
+  },
+  actions: {
+    // 增加list
+    addList(num: number) {
+      this.list.push(num)
+    },
+  },
+})
