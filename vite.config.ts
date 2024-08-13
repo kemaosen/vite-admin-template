abd87779e692df45c38859ec7d1ecebb04a0e660
Kms <kms@inesa-t.com>
2024-08-13 11:10:53 +0800

    chore(env): 🐳 配置环境变量

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve('./src'), // 设置别名
+export default ({ mode }: any) => {
+  // 获取配置环境参数
+  const env = loadEnv(mode, process.cwd())
+  console.log(env)
+  return defineConfig({
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'), // 设置别名
+      },
     },
-  },
-})
+  })
+}
