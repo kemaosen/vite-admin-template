b2a12696ecf97aff764c19289d04f0d9c11bfe68
Kms <kms@inesa-t.com>
2024-08-13 13:33:07 +0800

    upd(main): ⚡️ 添加css样式初始化

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { createApp } from 'vue'
-import './style.css'
+import 'normalize.css'
+import '@/styles/index.scss'
 import router from './router'
 import store from './store'
-import App from './App.vue'
+import App from './App'
 
 const app = createApp(App)
 app.use(store)
